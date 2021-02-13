7186e276be412e0b23b7291b2f7ed1f81987e0d1
Shravan Hariharan <shravanhariharan2@gmail.com>
2021-02-13 11:30:21 -0800

    Complete parts 1-4

--- components/product-item.js
+++ components/product-item.js
@@ -1,7 +1,37 @@
 // product-item.js
 
 class ProductItem extends HTMLElement {
-  // TODO
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.attachShadow({ mode: 'open' });
+
+    const listElement = document.createElement('li')
+    listElement.className = 'product';
+    
+    const imgElement = listElement.appendChild(document.createElement('img'));
+    imgElement.src = this.getAttribute('img');
+    imgElement.width = 200;
+
+    const pTitleElement = listElement.appendChild(document.createElement('p'));
+    const pPriceElement = listElement.appendChild(document.createElement('p'));
+    pTitleElement.className = 'title';
+    pPriceElement.className = 'price';
+    pTitleElement.textContent = this.getAttribute('title'); 
+    pPriceElement.textContent = this.getAttribute('price');
+
+    const buttonElement = listElement.appendChild(document.createElement('button'));
+    buttonElement.onclick = () => alert('Added to Cart!');
+    buttonElement.textContent = 'Add to cart';
+
+    const styleElement = document.createElement('link')
+    styleElement.setAttribute('rel', 'stylesheet');
+    styleElement.setAttribute('href', '/styles/styles.css');
+
+    this.shadowRoot.append(listElement, styleElement);
+  }
 }
 
 customElements.define('product-item', ProductItem);
--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,20 @@
 // Script.js
 
-window.addEventListener('DOMContentLoaded', () => {
-  // TODO
+window.addEventListener('DOMContentLoaded', async () => {
+  if(!window.localStorage.getItem('products')) {
+    const productResponse = await fetch('https://fakestoreapi.com/products');
+    const products = await productResponse.json();
+    window.localStorage.setItem('products', JSON.stringify(products));
+  }
+  const products = JSON.parse(window.localStorage.getItem('products'));
+  const productListElement = document.getElementById('product-list');
+
+  products.forEach((product) => {
+    const productElement = document.createElement('product-item');
+    productElement.setAttribute('img', product.image);
+    productElement.setAttribute('title', product.title);
+    productElement.setAttribute('price', product.price);
+    productListElement.appendChild(productElement);
+  });
+
 });
