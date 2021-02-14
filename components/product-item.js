451eb1a9862dca73bd2e431d75579336cf31a255
Shravan Hariharan <shravanhariharan2@gmail.com>
2021-02-13 18:27:12 -0800

    Complete parts 5-6

--- components/product-item.js
+++ components/product-item.js
@@ -6,31 +6,95 @@ class ProductItem extends HTMLElement {
   }
 
   connectedCallback() {
-    this.attachShadow({ mode: 'open' });
+    this.loadDOMElements();
+  }
 
+  loadDOMElements() {
+    this.attachShadow({ mode: 'open' });
+    this.renderCartSize();
+
+    const listElement = this.createListElement();
+    
+    this.createImageElement(listElement);
+    this.createTitleElement(listElement);
+    this.createPriceElement(listElement);
+    this.createButtonElement(listElement);
+
+    const styleElement = this.createStyles();
+
+    this.shadowRoot.append(listElement, styleElement);
+  }
+
+  renderCartSize() {
+    document.getElementById('cart-count').innerText = window.localStorage.getItem('cartSize');
+  }
+
+  createListElement() {
     const listElement = document.createElement('li')
     listElement.className = 'product';
-    
+    return listElement;
+  }
+
+  createImageElement(listElement) {
     const imgElement = listElement.appendChild(document.createElement('img'));
     imgElement.src = this.getAttribute('img');
     imgElement.width = 200;
+    return imgElement;
+  }
 
+  createTitleElement(listElement) {
     const pTitleElement = listElement.appendChild(document.createElement('p'));
-    const pPriceElement = listElement.appendChild(document.createElement('p'));
     pTitleElement.className = 'title';
+    pTitleElement.textContent = this.getAttribute('title');
+    return pTitleElement;
+  }
+
+  createPriceElement(listElement) {
+    const pPriceElement = listElement.appendChild(document.createElement('p'));
     pPriceElement.className = 'price';
-    pTitleElement.textContent = this.getAttribute('title'); 
     pPriceElement.textContent = this.getAttribute('price');
+    return pPriceElement;
+  }
 
+  createButtonElement(listElement) {
     const buttonElement = listElement.appendChild(document.createElement('button'));
-    buttonElement.onclick = () => alert('Added to Cart!');
-    buttonElement.textContent = 'Add to cart';
+    const hasCurrentItemInCart = window.localStorage.getItem(this.getAttribute('id')) === null ? false : true;
+    buttonElement.textContent = hasCurrentItemInCart ? 'Remove from cart' : 'Add to cart';
+    buttonElement.onclick = () => hasCurrentItemInCart ? this.removeCurrentItemFromCart(buttonElement) : this.addCurrentItemToCart(buttonElement);
+    return buttonElement;
+  }
 
+  addCurrentItemToCart(buttonElement) {
+    window.localStorage.setItem(this.getAttribute('id'), true);
+    this.incrementCartSize();
+    alert('Added to Cart!');
+    buttonElement.textContent = 'Remove from cart';
+    buttonElement.onclick = () => this.removeCurrentItemFromCart(buttonElement);
+  }
+
+  removeCurrentItemFromCart(buttonElement) {
+    window.localStorage.removeItem(this.getAttribute('id'));
+    this.decrementCartSize();
+    alert('Removed from cart!');
+    buttonElement.textContent = 'Add to cart';
+    buttonElement.onclick = () => this.addCurrentItemToCart(buttonElement);
+  }
+
+  incrementCartSize() {
+    window.localStorage.setItem('cartSize', Number(window.localStorage.getItem('cartSize')) + 1);
+    this.renderCartSize();
+  }
+
+  decrementCartSize() {
+    window.localStorage.setItem('cartSize', Number(window.localStorage.getItem('cartSize')) - 1);
+    this.renderCartSize();
+  }
+
+  createStyles() {
     const styleElement = document.createElement('link')
     styleElement.setAttribute('rel', 'stylesheet');
     styleElement.setAttribute('href', '/styles/styles.css');
-
-    this.shadowRoot.append(listElement, styleElement);
+    return styleElement;
   }
 }
 
--- scripts/script.js
+++ scripts/script.js
@@ -6,11 +6,16 @@ window.addEventListener('DOMContentLoaded', async () => {
     const products = await productResponse.json();
     window.localStorage.setItem('products', JSON.stringify(products));
   }
+
+  if(!window.localStorage.getItem('cartSize')) {
+    window.localStorage.setItem('cartSize', 0)
+  }
   const products = JSON.parse(window.localStorage.getItem('products'));
   const productListElement = document.getElementById('product-list');
 
   products.forEach((product) => {
     const productElement = document.createElement('product-item');
+    productElement.setAttribute('id', product.id);
     productElement.setAttribute('img', product.image);
     productElement.setAttribute('title', product.title);
     productElement.setAttribute('price', product.price);
